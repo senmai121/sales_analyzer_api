c006baf6a1eeb8fd728838286fd29a86849dad9f
senmai121 <kanachai.niyom@gmail.com>
2026-04-23 19:52:14 +0700

    remove comment

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -28,7 +27,6 @@ func main() {
 	if jwtSecret == "" {
 		log.Fatal("JWT_SECRET environment variable is required")
 	}
-	fmt.Println(jwtSecret)
 
 	ctx := context.Background()
 
